a3c17969088ec08778aba4b5ea117b7946cac65e
Tommaso Visconti <tommaso.visconti@gmail.com>
2016-10-04 23:36:55 +0200

    Add CORS support to Phoenix

--- api_server/web/router.ex
+++ api_server/web/router.ex
@@ -2,11 +2,14 @@ defmodule WheredafuckruApi.Router do
   use WheredafuckruApi.Web, :router
 
   pipeline :api do
+      plug CORSPlug, [origin: "http://localhost:8080"]
     plug :accepts, ["json"]
   end
 
   scope "/api", WheredafuckruApi do
     pipe_through :api
     resources "/positions", PositionController, except: [:new, :edit]
+    options   "/positions", ArticleController, :options
+    options   "/positions/:id", ArticleController, :options
   end
 end
